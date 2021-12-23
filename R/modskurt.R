6a3a95a44ba4464fad335c50faaa3b9461ae63d3
andrew-primer-e <andrew@primer-e.co.nz>
2021-12-23 15:54:01 +1300

    Add functions to retrieve the stan code.

--- R/modskurt.R
+++ R/modskurt.R
@@ -12,6 +12,17 @@ modskurtp1d0r05 <- function(x, H, m, s) {
   modskurt(x, H, m, s, 0.5, 0, 1)
 }
 
+#' Symmetric Modskurt Stan code.
+#'
+#' Stan code for the symmetric Modskurt.
+#'
+#' @usage NULL
+#' @format NULL
+#' @export
+#'
+#' @examples
+#' cat(modskurtp1d0r05_stan[["source_code"]])
+modskurtp1d0r05_stan <- get_stan_function("modskurtp1d0r05")
 
 
 #' Symmetric Modskurt with varying flattening / peakedness
@@ -28,6 +39,18 @@ modskurtp1r05 <- function(x, H, m, s, d) {
   modskurt(x, H, m, s, 0.5, d, 1)
 }
 
+#' Symmetric Modskurt with varying flattening / peakedness Stan code.
+#'
+#' Stan code for the symmetric Modskurt with varying flattening / peakedness.
+#'
+#' @usage NULL
+#' @format NULL
+#' @export
+#'
+#' @examples
+#' cat(modskurtp1r05_stan[["source_code"]])
+modskurtp1r05_stan <- get_stan_function("modskurtp1r05")
+
 #' Asymetric Modskurt
 #'
 #' @param x Environmental variable
@@ -42,7 +65,19 @@ modskurtp1d0 <- function(x, H, m, s, r) {
   modskurt(x, H, m, s, r, 0, 1)
 }
 
-#' Asymetric with varied flattening / peakedness
+#' Asymmetric modskurt Stan code.
+#'
+#' Stan code for the modskurt with asymmetry
+#'
+#' @usage NULL
+#' @format NULL
+#' @export
+#'
+#' @examples
+#' cat(modskurtp1d0_stan[["source_code"]])
+modskurtp1d0_stan <- get_stan_function("modskurtp1d0")
+
+#' Modskurt with Asymmetry and varied flattening / peakedness.
 #'
 #' @param x Environmental variable
 #' @param H Height of the mode
@@ -57,7 +92,19 @@ modskurtp1 <- function(x, H, m, s, r, d) {
   modskurt(x, H, m, s, r, d, 1)
 }
 
-#' Asymetric allowing "exaggeration" / large tails
+#' Modskurt with Asymmetry and varied flattening / peakedness Stan code.
+#'
+#' Stan code for the modskurt with asymmetry and varied flattening / peakedness
+#'
+#' @usage NULL
+#' @format NULL
+#' @export
+#'
+#' @examples
+#' cat(modskurtp1_stan[["source_code"]])
+modskurtp1_stan <- get_stan_function("modskurtp1")
+
+#' Modskurt with asymmetry allowing "exaggeration" / large tails
 #'
 #' @param x Environmental variable
 #' @param H Height of the mode
@@ -71,6 +118,18 @@ modskurtp1 <- function(x, H, m, s, r, d) {
 modskurtd0 <- function (x, H, m, s, r, p) {
   modskurt(x, H, m, s, r, 0, p);
 }
+
+#' Modskurt with asymmetry allowing "exaggeration" / large tails Stan code.
+#'
+#' Stan code for the modskurt with asymmetry allowing "exaggeration" / large tails.
+#'
+#' @usage NULL
+#' @format NULL
+#' @export
+#'
+#' @examples
+#' cat(modskurtd0_stan[["source_code"]])
+modskurtd0_stan <- get_stan_function("modskurtd0")
 
 #' Full modskurt
 #'
@@ -89,7 +148,7 @@ modskurt <- function(x, H, m, s, r, d, p) {
          (1 - r) * exp(- ((x - m)/((1 - r) * s) + d) / p) - exp(- d / p) + 1) ^ p
 }
 
-#' Modskurt Stan Code
+#' Full modskurt Stan code
 #'
 #' Stan code for the full modskurt function
 #'
