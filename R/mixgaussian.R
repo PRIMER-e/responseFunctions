4b2baf8631d6cf248eec3e4157a7daf554d35303
andrew-primer-e <andrew@primer-e.co.nz>
2022-05-23 12:50:47 +1200

    Add an equation to the mixgaussian docs.

--- R/mixgaussian.R
+++ R/mixgaussian.R
@@ -1,5 +1,10 @@
 #' Mixture of two Gaussian functions.
 #'
+#' R version.
+#' \deqn{f(x; H_1, H_2, m_1, m_2, s_1, s_2) =
+#'       H_1 \exp \left( -\frac{1}{2} \left( \frac{x - m_1}{s_1} \right) ^ 2 \right) +
+#'       H_2 \exp \left( -\frac{1}{2} \left( \frac{x - m_2}{s_2} \right) ^ 2 \right)}{}
+#'
 #' @param x Environmental variable
 #' @param H1 Height of the first Gaussian
 #' @param H2 Height of the second Gaussian
